d49436818318d5869dc9ab48eea266dec5081927
Reuben Kogie <kogie.reuben@gmail.com>
2021-04-11 13:01:39 -0600

    added comments for time complexity

--- Python/heap_sort.py
+++ Python/heap_sort.py
@@ -3,6 +3,7 @@ def heapify(arr, n, i):
     This function takes the built heap and checks the individual leaf nodes against the parent
     and assesses if they satisfy the condition that parent must be greater than or equal to each of
     its children. If true, traverse to the next level (subtree), however if false, swap the nodes
+    Time complexity is O(log n) due to tree traversal reducing n to n/2 on each level
     """
     largest = i  # assign your index passed in as your root node (largest)
     left = 2 * i + 1  # left leaf
@@ -24,6 +25,12 @@ def heapify(arr, n, i):
 
 
 def heap_sort(arr):
+    """
+    Overall time complexity of heap sort is O(n log n)
+    This is due to the O(n) time it takes to create the initial heap (using for loop to iterate over the size of the array)
+    Then calling the heapify() on each iteration and performs O(log n) operations due to ascending up each layer of the tree in log time
+    as the leafs reduce in n/2 on each layer until the root element = 1 item
+    """
     size = len(arr)  # get size of array
 
     # Build a max-heap (largest number is the root)
