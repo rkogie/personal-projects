fc759c73a8a8264a15338941a6d46253f5be85fb
Reuben Kogie <kogie.reuben@gmail.com>
2021-09-23 14:38:16 -0600

    built react front end - default app

---
+++ 3_C# Projects/SocialMediaApp/client-app/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
+
+function App() {
+
+  const [activities, setActvities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(res => {
+      console.log(res);
+      setActvities(res.data);
+    })
+  }, [])
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='SocialMediaApp' />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id} >
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
